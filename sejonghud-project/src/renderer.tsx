d9a765c1f78d37131b340f9157f4bed42992add1
ehrmantraut5 <sejunkwon@outlook.com>
2023-11-24 21:15:20 +0900

    project rebase

--- sejonghud-project/src/renderer.tsx
+++
@@ -1,9 +0,0 @@
-const {ipcRenderer} = require('electron')
-
-window.onload = () => {
-    const btnEl = document.getElementById('btn')
-
-    btnEl?.addEventListener('click', (evt) => {
-        const inputValue = document.getElementById('text-input').value
-    })
-}
