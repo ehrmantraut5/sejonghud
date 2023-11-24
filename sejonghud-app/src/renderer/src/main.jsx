0759e9b06a4bb112caf85b7c176d90ee8d399ead
ehrmantraut5 <sejunkwon@outlook.com>
2023-11-25 00:21:51 +0900

    dev11/25-00:21

--- sejonghud-app/src/renderer/src/main.jsx
+++ sejonghud-app/src/renderer/src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './assets/index.css'
 import App from './App'
+import { BrowserRouter } from "react-router-dom"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+/*ReactDOM.createRoot(document.getElementById('root'))*/
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 )
