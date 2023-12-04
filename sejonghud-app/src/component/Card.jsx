6e522c64691a01928af6e0cd97f480c8d49a788c
ehrmantraut5 <sejunkwon@outlook.com>
2023-12-04 23:06:45 +0900

    dev-4-dec-23:08

---
+++ sejonghud-app/src/component/Card.jsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+
+export default function ActionAreaCard({title}) {
+  return (
+    <Card sx={{ minWidth:345, maxWidth: 345, mt: 2, mr: 2, ml: 2, mb: "2px"}}>
+      <CardActionArea>
+        {/* <CardMedia
+          component="img"
+          height="140"
+          image="/static/images/cards/contemplative-reptile.jpg"
+          alt="green iguana"
+        /> */}
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
---
+++ sejonghud-app/src/data/SubjectData.js
@@ -0,0 +1,1 @@
+export const Subjects = ['Computer Architecture', 'Advanced C Programming and Lab', 'Basic Design', 'Algorithm and Lab','Introduction to Open Source Software', 'Linear Algebra and Programming'];
